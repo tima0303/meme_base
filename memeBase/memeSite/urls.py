83cc506028f44e08e87ad2fbc6cdbd24495f168c
Яшка <holyyakoff@gmail.com>
2022-05-04 12:27:07 +0700

    Добавлен поиск по названию мема

--- memeBase/memeSite/urls.py
+++ memeBase/memeSite/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', MemeHome.as_view(), name='home'),
     path('guide/', guide, name='guide'),
+    path('search/', SearchResultView.as_view(), name='search_results')
 ]
--- memeBase/memeSite/views.py
+++ memeBase/memeSite/views.py
@@ -1,17 +1,31 @@
 from django.http import HttpResponseNotFound
 
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from meme.models import Meme
 
 
-def index(request):
-    """
-    Фун-я перенаправляющая на галвную страницу
-    :param request:
-    """
-    meme = Meme.objects.order_by('-time_create')
-    return render(request, 'memeSite/index.html', {'meme': meme})
+class MemeHome(ListView):
+    model = Meme
+    template_name = "memeSite/index.html"
+    context_object_name = "meme"
+
+
+class SearchResultView(ListView):
+    model = Meme
+    template_name = "memeSite/search_results.html"
+    context_object_name = "meme"
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        object_list = Meme.objects.filter(title__icontains=query)
+        return object_list
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Ищем мемы'
+        return context
 
 
 def guide(request):
