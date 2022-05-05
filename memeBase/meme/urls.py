48db3e7b52708ec938c53bc4a1c680039f31b022
Яшка <holyyakoff@gmail.com>
2022-05-05 14:03:26 +0700

    Доработаны закладки

--- memeBase/meme/urls.py
+++ memeBase/meme/urls.py
@@ -6,5 +6,5 @@ from .views import *
 urlpatterns = [
     path('manage/', manage, name='manage'),
     path('create/', create, name='create'),
-    path('<int:pk>', MemeDetailView.as_view(), name='meme-detail')
+    path('<int:id>', meme_detail_view, name='meme-detail')
 ]
--- memeBase/meme/views.py
+++ memeBase/meme/views.py
@@ -7,23 +7,12 @@ from django.views.generic import DetailView
 from meme.models import Meme
 
 
-class MemeDetailView(DetailView):
-    model = Meme
-    template_name = 'meme/details_view.html'
-    context_object_name = 'meme'
-
-    # def get_context_data(self, *, object_list=None, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['fav'] = Meme.objects.order_by().values('tag').distinct()
-    #     return context
-
-
-# def meme_detail_view(request, id):
-#     meme = get_object_or_404(Meme, status='is_published')
-#     fav = bool
-#     if meme.favorites.filter(id=request.user.id).exists():
-#         fav = True
-#     return render(request, 'meme/details_view.html', {'meme': meme, 'fav': fav})
+def meme_detail_view(request, id):
+    meme = get_object_or_404(Meme, id=id)
+    fav = bool
+    if meme.favorites.filter(id=request.user.id).exists():
+        fav = True
+    return render(request, 'meme/details_view.html', {'meme': meme, 'fav': fav})
 
 
 def manage(request):
